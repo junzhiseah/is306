8ce8098306af9c953a492bbe4ae7860fba55c262
Jeff <cs.tan.2011@sis.smu.edu.sg>
2014-03-04 18:25:08 +0800

    mainpage

--- app/views/try.php
+++
@@ -1,1 +0,0 @@
-<html>testing</html>
---
+++ index.php
@@ -0,0 +1,56 @@
+<style type="text/css">
+	body{
+	background-image: url('img/S4.png');
+	/*background-size: 100%;*/
+
+	background-repeat: no-repeat;
+	
+}
+
+
+div.maincontainer{
+	border:0px solid black;
+	margin-left: 30%;
+	margin-right: 46.9%;
+	margin-top: 6%;
+	height:77%;
+	background-color: white;
+	overflow:auto;
+
+}
+div.categories{
+	display: inline;
+}
+
+div.categories2{
+	display: inline;
+}
+
+img.category{
+	width:146px;
+	height:146px;
+}
+
+
+</style>
+
+<div class="maincontainer">
+test
+
+<br/>
+
+<div class="categories1">
+<img src="img/Aeroplane.jpg" class="category">
+<img src="img/Aeroplane.jpg" class="category">
+</div>
+
+
+<div class="categories2">
+<img src="img/Aeroplane.jpg" class="category">
+<img src="img/Aeroplane.jpg" class="category">
+</div>
+
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+testing
+
+</div>
