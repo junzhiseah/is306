408e3f25f717df67d95a448fb49e045c76fcd51c
Jeff <cs.tan.2011@sis.smu.edu.sg>
2014-03-04 18:38:56 +0800

    test

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <style type="text/css">
 	body{
-	background-image: url('img/S4.png');
-	/*background-size: 100%;*/
+		background-image: url('img/S4.png');
+		/*background-size: 100%;*/
 
-	background-repeat: no-repeat;
-	
-}
+		background-repeat: no-repeat;
+		
+	}
 
 
 div.maincontainer{
 	border:0px solid black;
-	margin-left: 30%;
-	margin-right: 46.9%;
+	/*margin-left: 30%;
+	margin-right: 46.9%;*/
+	margin-left: 32%;
+	margin-right:43.6%;
 	margin-top: 6%;
-	height:77%;
+	height:79%;
 	background-color: white;
 	overflow:auto;
 
@@ -27,30 +29,36 @@ div.categories2{
 }
 
 img.category{
-	width:146px;
-	height:146px;
+	width:143px;
+	height:143px;
 }
 
 
 </style>
 
 <div class="maincontainer">
-test
+	test
 
-<br/>
+	<br/>
 
-<div class="categories1">
-<img src="img/Aeroplane.jpg" class="category">
-<img src="img/Aeroplane.jpg" class="category">
-</div>
+	<div class="navbar">
+
+	</div>	
 
 
-<div class="categories2">
-<img src="img/Aeroplane.jpg" class="category">
-<img src="img/Aeroplane.jpg" class="category">
-</div>
+	<div class="categories1">
+	<img src="img/Aeroplane.jpg" class="category">
+	<img src="img/Aeroplane.jpg" class="category">
+	</div>
 
-<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-testing
+
+	<div class="categories2">
+	<img src="img/Aeroplane.jpg" class="category">
+	<img src="img/Aeroplane.jpg" class="category">
+	</div>
+
+
+	<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+	testing
 
 </div>
