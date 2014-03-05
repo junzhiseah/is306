f76e128f7280285a08c79940574fdc42b177a4f4
Rayner <rayner.koh.2011@sis.smu.edu.sg>
2014-03-05 14:45:47 +0800

    Rayner Commit

---
+++ featured.php
@@ -0,0 +1,90 @@
+<style type="text/css">
+	body{
+		background-image: url('img/S4.png');
+		/*background-size: 100%;*/
+
+		background-repeat: no-repeat;
+		
+	}
+
+
+div.maincontainer{
+	border:0px solid black;
+	/*margin-left: 30%;
+	margin-right: 46.9%;*/
+	margin-left: 32%;
+	margin-right:43.6%;
+	margin-top: 6%;
+	height:77%;
+	background-color: white;
+	overflow:auto;
+
+}
+div.categories{
+	display: inline;
+}
+
+div.categories2{
+	display: inline;
+}
+
+img.category{
+	width:143px;
+	height:143px;
+}
+
+
+</style>
+
+
+
+<div class="maincontainer">
+	
+
+	<br/>
+
+	<div class="navbar">
+
+	</div>	
+
+	<table border = "1" width = "293px">
+		<tr>
+			<td align = "center" bgcolor = "#1D69CD" width = "73.25px"><a style="color:white"><b>Featured</b></a></td>
+			<td align = "center" bgcolor = "#2C7EE9" width = "73.25px"><a style="color:white">Entertainment</a></td>
+			<td align = "center" bgcolor = "#2C7EE9" width = "73.25px"><a style="color:white">Food</a></td>
+			<td align = "center" bgcolor = "#2C7EE9" width = "73.25px"><a style="color:white">Travel</a></td>
+
+		</tr>
+	</table>
+	<table border = "1">
+		<tr>
+			<td align = "center" ><img src="img/IceArt.jpg" class="category"></td>
+			<td align = "center"><b>2 Degree Ice Art</b><br>
+				<strike>$30</strike> <a style="color:red">$15</a> / Tix<br>
+				<i>Expiry: 27 Mar 2014</i><br>
+				<img src="img/BuyNow.jpg" height="35px" width = "65px"><img src="img/DealInfo.jpg" height="35px" width = "65px">
+			</td>
+		</tr>
+		<tr>
+			<td align = "center" ><img src="img/LionKing.jpg" class="category"></td>
+			<td align = "center"><b>Lion King Musical</b><br>
+				<strike>$75</strike> <a style="color:red">$25</a> / Tix<br>
+				<i>Expiry: 28 Mar 2014</i>
+				<img src="img/BuyNow.jpg" height="35px" width = "65px"><img src="img/DealInfo.jpg" height="35px" width = "65px">
+			</td>
+		</tr>
+		<tr>
+			<td align = "center" ><img src="img/Korea.jpg" class="category"></td>
+			<td align = "center"><b>One way Travel Tix to Korea</b><br>
+				<strike>$478</strike> <a style="color:red">$128</a> / Tix<br>
+				<i>Expiry: 27 Mar 2014</i>
+				<img src="img/BuyNow.jpg" height="35px" width = "65px"><img src="img/DealInfo.jpg" height="35px" width = "65px">
+			</td>
+		</tr>
+
+	</table>
+
+	<br>
+	<br>
+
+</div>
