85c8059b3fb95971b87340abd727495ed72297c7
Jeff <cs.tan.2011@sis.smu.edu.sg>
2014-03-08 22:09:17 +0800

    navbar menu

--- samsung.php
+++ samsung.php
@@ -37,6 +37,7 @@
 		margin-right: 43.4%;
 		margin-top: 6.2%;
 		background-color:#3E4651;
+
 	}
 
 	div.navbar img.homepageicon{
@@ -46,65 +47,97 @@
 	}	
 
 	div.navbar img.menu{
-		margin-left:57%;
+		margin-left:10px;
 		width:40px;
 		height:40px;
-		margin-bottom:1.5%; 
+		margin-bottom:20px; 
+		margin-top: -10px;
 	}
 
 
 
 
-	nav a img ul {
+
+
+
+	nav ul ul {
 		display: none;
 	}
 
-	nav a img:hover > ul li {
+	nav ul li:hover > ul {
 		display: block;
+		/*margin-bottom: 20px;*/
 	}
 
 
-	ul.nav.navbar-nav.navbar-right{
-	    margin-top: auto;
-	    padding-right: 10px;
-	    font-size: 10pt !important;
+	nav ul {
+	padding: 0 0px;
+	border-radius: 10px;  
+	list-style: none;
+	position: relative;
+	display: inline-table;
 	}
-
-	ul.nav.navbar-right li.dropdown{
-	    display:inline-block !important;
-	   
-	   /* margin-left:1000px;*/
-	}
-
-
-	li.dropdown a.dropdown-toggle{
-	    height:59px;
-	    background-color:none;   
-	    padding-top:5px !important; 
-	    padding-left:5px !important;
-	    float:right;
+	
+	nav ul:after {
+		content: ""; clear: both; display: block;
 	}
 
 
 
-	li.dropdown a.dropdown-toggle:hover{
-	    height:59px;
-	    background-color:#B8DBFF;
+	nav ul li {
+	float: right;
+	margin-left: 140px;
+	}
 
+	nav ul li ul li{
+		margin-left: 0px;
+	}
+
+	nav ul li:hover {
+		background: #4b545f;
+		background: linear-gradient(top, #4f5964 0%, #5f6975 40%);
+		background: -moz-linear-gradient(top, #4f5964 0%, #5f6975 40%);
+		background: -webkit-linear-gradient(top, #4f5964 0%,#5f6975 40%);
+	}
+		
+	nav ul li:hover a {
+		color: #fff;
+	}
+	
+	nav ul li a {
+		display: block; /*padding: 13px 10px 0px 10px;*/
+		padding:0px;
+		color: #757575; text-decoration: none;
+		border:0px solid black;
+		margin-top: -10px;
+		/*height:40px;*/
+
+	/*	margin-left: 120px;*/
 	}
 
 
-	li.dropdown a.dropdown-toggle:visited{
-	    height:59px;
-	    background-color:none;
-
+	nav ul ul {
+	background: #5f6975; border-radius: 0px; padding: 0;
+	position: absolute; top: 100%;
 	}
 
-	.dropdown-menu{
-	    /*background-image: url('../img/white.jpg');*/
-	    background-color:#B8DBFF !important;
-	    float:right;
+	nav ul ul li {
+		float: none; 
+		border-top: 1px solid #6b727c;
+		border-bottom: 1px solid #575f6a;
+		position: relative;
 	}
+
+	nav ul ul li a {
+		padding: 15px 20px 15px 20px;
+		width:70px;
+		color: #fff;
+	}	
+		
+	nav ul ul li a:hover {
+		background: #4b545f;
+	}
+
 
 
 
@@ -122,25 +155,41 @@
 				
 
 
-				<ul class="nav navbar-nav navbar-right">
-					<li class="dropdown">
+				<ul>
+				<li>
 
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="img/Table-of-Contents-48.png" class="menu"/>
-						
-				</a>	
+					<a href="#" data-toggle="dropdown"><img src="img/Table-of-Contents-48.png" class="menu"/></a>	
 
 
-				<ul class="dropdown-menu">
-                  <li><a href="#">My Profile</a></li>
-                  <li><a href="#">Events</a></li>
-                  <li><a href="#">Settings</a></li>
-                  <li><a href="#" >Log Out</a></li>
-                </ul>
-              </li>
-             </ul>
+					<ul class="dropdown-menu">
+	                  <li><a href="#">My Profile</a></li>
+	                  <li><a href="#">Events</a></li>
+	                  <li><a href="#">Settings</a></li>
+	                  <li><a href="#" >Log Out</a></li>
+	                </ul>
+
+             	</li>
+             	</ul>
                	
 		</nav>		
 	</div>	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	<div id="iframe-container">
 
