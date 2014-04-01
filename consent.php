60728faff813b73ceb0904278102df4ca93b46d8
Rayner <rayner.koh.2011@sis.smu.edu.sg>
2014-04-01 21:56:48 +0800

    ggg

---
+++ consent.php
@@ -0,0 +1,16 @@
+<form method="get" action="https://usabilityhub.com/do/7b1375ff35cb/31c0">
+
+	Consent details to be filled in here
+
+	<br/>
+	.....
+
+	<br/>
+
+
+	After you are done with the web experiment, please filled up the post questionnaire that is attached together with the email.
+
+	<br/>
+	<input type="submit" value="Agree to do the Web Experiment" />
+</form>
+
---
+++ intro.php
@@ -0,0 +1,13 @@
+<form method="get" action="consent.php">
+
+	Introduction about the web experiment
+	<br/>
+	....
+	<br/>
+	
+	....
+
+	<br/>
+	<input type="submit" value="Next" />
+</form>
+
