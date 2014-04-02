f36fe284ccca24803b946807c73f30aca237792e
Jeff <cs.tan.2011@sis.smu.edu.sg>
2014-04-02 20:55:40 +0800

    split intro and consent into a and b

--- consentA.php
+++ consentA.php
@@ -1,18 +1,28 @@
+
+
+
+
+
+
+
+
 <style type="text/css">
 
 	div.maincontainer{
 		text-align: left;
+		height:80%;
 		margin-left:20%;
 		margin-right:20%;
-		margin-top: 3%;
-		padding-left: 9%;
-		padding-right: 9%;
+		margin-top: 2%;
+		padding-left: 7%;
+		padding-right: 7%;
 		padding-top: 1%;
-		padding-bottom: 2%;
+		padding-bottom: 3%;
 		color: #3E4651;
 		font-size:17px;
 		font-weight: normal;
 		 background-color: rgba(222,213,204,0.7);
+		 overflow:auto;
 
 	}
 
@@ -58,7 +68,7 @@
 		line-height:25px;
 		margin-right: 0px;
 		margin-left: 0px;
-		width:80px;
+		width:185px;
 		text-decoration:none;
 		text-align:center;
 		text-shadow:1px 1px 0px #3E4651;
@@ -85,40 +95,50 @@
 
 
 
+<form method="get" action="https://usabilityhub.com/do/7b1375ff35cb/31c0">
 
-<form method="get" action="consent.php">
+<div class="maincontainer">	
 
-		
-	<div class="maincontainer">
+<h1 class="title">Web Experiment Consent</h1>
+
+</p>
+April 02, 2014
+
+</p>
+
+You are being invited to participate in a web experiment about the usability of a mobile application.
+We are a team from the Singapore Management University, and this research project is conducted for our Interaction and Design Prototyping module.
+The objective of this web experiment is to attempt to find out the usability of our application. 
+
+</p>
+The purpose of this study is not a test of your abilities, but to test the usability of our system.
+You can choose to quit this web experiment at any time. 
+</p>
+
+There are no known risks if you decide to participate in this web experiment, nor are there any costs for participating in the experiment.
+
+</p>
+The information you provide will help us to understand how best to satisfy the needs of potential users of this application. 
+The information collected may not benefit you directly, but what we learn from this study will greatly help us to enhance the user friendliness of our application.
+
+</p>
+A short post test questionaire will be collected from you after the experiment.
+If you choose to remain anonymous, you may wish to leave out your name in the post test questionaire.
+Nothing you say on the questionnaire will in any way affect you.
+
+</p>
+If you have any questions or concerns in the midst of the web experiment, you may choose to contact Rayner at 92218237 (rayner.koh.2011@sis.smu.edu.sg) or Jeff at 94889695 (cs.tan.2011@sis.smu.edu.sg).
 
 
-	<h1 class="title">Introduction</h1>
-	
-	<br/>
-
-	Hi guys! <p/>
-
-	Thank you for agreeing to participate in our online web experiment! </p>
-	The purpose of this study is not a test of your abilities, but to test the usability of our system.
-	You can choose to quit this web experiment at any time. 
-
-	<p/>
-
-	A short introduction on our app. 
-	The app is called "Lei Long", and is catered for last minute deals and promotions. It is a platform for merchants to publicise their deals effectively. 
-	
-	We would like you to take the role of a customer, and you will need to locate the various information of a deal according to our web experiment instructions. 
-	The web experiment will be done on Navflow.
-	
+ 
 
 
-	<p/>
-
-	Please click the button to proceed to our consent page.</p>
-
-	<input type="submit" value="Next" />
-
+	<br/><br/>
+	<input type="submit" value="Proceed to Web Experiment" />
 
 </div>
+
+
 </form>
+
 
